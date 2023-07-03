28ee7e31e1f304566e5cab36cb7d9549821aa126
Akashfw <shuklaakash30278@gmail.com>
2023-07-03 11:57:45 +0530

    zomato-2

--- python/zomato/index.py
+++ python/zomato/index.py
@@ -7,15 +7,47 @@ class Dish:
         self.price = price
         self.availability = availability
 
+    def to_dict(self):
+        return {
+            'dish_id': self.dish_id,
+            'dish_name': self.dish_name,
+            'price': self.price,
+            'availability': self.availability
+        }
+
+    @staticmethod
+    def from_dict(data):
+        dish_id = data['dish_id']
+        dish_name = data['dish_name']
+        price = data['price']
+        availability = data['availability']
+        return Dish(dish_id, dish_name, price, availability)
+
 class Order:
-    def __init__(self, order_id, customer_name, dishes):
+    def __init__(self, order_id, customer_name, dishes, status='received'):
         self.order_id = order_id
         self.customer_name = customer_name
         self.dishes = dishes
-        self.status = 'received'
+        self.status = status
 
     def calculate_total_price(self):
         return sum(dish.price for dish in self.dishes)
+
+    def to_dict(self):
+        return {
+            'order_id': self.order_id,
+            'customer_name': self.customer_name,
+            'dishes': [dish.__dict__ for dish in self.dishes],
+            'status': self.status
+        }
+
+    @staticmethod
+    def from_dict(data):
+        order_id = data['order_id']
+        customer_name = data['customer_name']
+        dishes = [Dish.from_dict(dish_data) for dish_data in data['dishes']]
+        status = data['status']
+        return Order(order_id, customer_name, dishes, status)
 
 class ZestyZomato:
     def __init__(self):
@@ -42,6 +74,11 @@ class ZestyZomato:
                 print("Dish availability updated.")
                 return
         print("Dish not found in the menu.")
+
+    def view_menu(self):
+        print("Menu:")
+        for dish in self.menu:
+            print(f"Dish ID: {dish.dish_id}, Name: {dish.dish_name}, Price: {dish.price}, Availability: {dish.availability}")
 
     def take_order(self, customer_name, dish_ids):
         dishes = []
@@ -75,111 +112,106 @@ class ZestyZomato:
             if status is None or order.status.lower() == status.lower():
                 print(f"Order ID: {order.order_id}, Customer: {order.customer_name}, Status: {order.status}")
 
-    def calculate_total_price(self, order_id):
-        for order in self.orders:
-            if order.order_id == order_id:
-                total_price = order.calculate_total_price()
-                print(f"Total price of Order ID {order_id}: {total_price}")
-                return
-        print("Order not found.")
+    def calculate_total_price(self):
+        total_price = sum(order.calculate_total_price() for order in self.orders)
+        print(f"Total price of all orders: {total_price}")
 
-    def save_data(self):
-        data = {
-            'menu': [dish.__dict__ for dish in self.menu],
-            'orders': [order.__dict__ for order in self.orders],
-            'order_counter': self.order_counter
-        }
-        with open('zesty_zomato_data.json', 'w') as file:
-            json.dump(data, file)
-        print("Data saved successfully.")
+    def save_data(self, menu_file, orders_file):
+        menu_data = [dish.to_dict() for dish in self.menu]
+        with open(menu_file, 'w') as f:
+            json.dump(menu_data, f, indent=4)
+        print("Menu data saved.")
 
-    def load_data(self):
+        orders_data = [order.to_dict() for order in self.orders]
+        with open(orders_file, 'w') as f:
+            json.dump(orders_data, f, indent=4)
+        print("Orders data saved.")
+
+    def load_data(self, menu_file, orders_file):
         try:
-            with open('zesty_zomato_data.json', 'r') as file:
-                data = json.load(file)
-                self.menu = [Dish(**dish_data) for dish_data in data['menu']]
-                self.orders = [Order(**order_data) for order_data in data['orders']]
-                self.order_counter = data['order_counter']
-            print("Data loaded successfully.")
-        except FileNotFoundError:
-            print("No previous data found.")
+            with open(menu_file, 'r') as f:
+                menu_data = json.load(f)
+                self.menu = [Dish.from_dict(dish_data) for dish_data in menu_data]
+            print("Menu data loaded.")
 
-def print_menu():
-    menu = """
-    ----------------------------
-    Zesty Zomato Menu Management
-    ----------------------------
-    1. Add a new dish to the menu
-    2. Remove a dish from the menu
-    3. Update dish availability
-    4. Take a new order
-    5. Update order status
-    6. Review all orders
-    7. Calculate total price of an order
-    8. View orders with a specific status
-    9. Save data
-    10. Load data
-    0. Exit
-    ----------------------------
-    """
-    print(menu)
+            with open(orders_file, 'r') as f:
+                orders_data = json.load(f)
+                self.orders = [Order.from_dict(order_data) for order_data in orders_data]
+                self.order_counter = max(order.order_id for order in self.orders) + 1
+            print("Orders data loaded.")
+        except FileNotFoundError:
+            print("Data files not found. Starting with an empty menu and no orders.")
+
+
+def display_options():
+    print("********** Zesty Zomato Menu **********")
+    print("1. Add a dish to the menu")
+    print("2. Remove a dish from the menu")
+    print("3. Update dish availability")
+    print("4. View the menu")
+    print("5. Take a new order")
+    print("6. Update order status")
+    print("7. Review all orders")
+    print("8. Calculate the total price of all orders")
+    print("9. Save dish and order data")
+    print("10. Load dish and order data")
+    print("11. Exit")
 
 
 def main():
     zomato = ZestyZomato()
-    zomato.load_data()
+    menu_file = 'menu.json'
+    orders_file = 'orders.json'
+    zomato.load_data(menu_file, orders_file)
 
     while True:
-        print_menu()
-        choice = input("Enter your choice: ")
+        display_options()
+        choice = input("Enter your choice (1-11): ")
 
-        if choice == "1":
+        if choice == '1':
             dish_id = int(input("Enter dish ID: "))
             dish_name = input("Enter dish name: ")
-            price = float(input("Enter dish price: "))
-            availability = input("Enter dish availability (yes/no): ")
+            price = float(input("Enter price: "))
+            availability = input("Enter availability (yes/no): ")
             dish = Dish(dish_id, dish_name, price, availability)
             zomato.add_dish(dish)
 
-        elif choice == "2":
+        elif choice == '2':
             dish_id = int(input("Enter dish ID to remove: "))
             zomato.remove_dish(dish_id)
 
-        elif choice == "3":
+        elif choice == '3':
             dish_id = int(input("Enter dish ID to update availability: "))
             availability = input("Enter new availability (yes/no): ")
             zomato.update_availability(dish_id, availability)
 
-        elif choice == "4":
+        elif choice == '4':
+            zomato.view_menu()
+
+        elif choice == '5':
             customer_name = input("Enter customer name: ")
-            dish_ids = [int(x) for x in input("Enter dish IDs (space-separated): ").split()]
+            dish_ids = [int(x) for x in input("Enter dish IDs (separated by commas): ").split(',')]
             zomato.take_order(customer_name, dish_ids)
 
-        elif choice == "5":
+        elif choice == '6':
             order_id = int(input("Enter order ID to update status: "))
             new_status = input("Enter new status: ")
             zomato.update_order_status(order_id, new_status)
 
-        elif choice == "6":
-            zomato.review_orders()
-
-        elif choice == "7":
-            order_id = int(input("Enter order ID to calculate total price: "))
-            zomato.calculate_total_price(order_id)
-
-        elif choice == "8":
-            status = input("Enter order status to filter (leave blank for all orders): ")
+        elif choice == '7':
+            status = input("Enter status to filter orders (leave blank to show all orders): ")
             zomato.review_orders(status)
 
-        elif choice == "9":
-            zomato.save_data()
+        elif choice == '8':
+            zomato.calculate_total_price()
 
-        elif choice == "10":
-            zomato.load_data()
+        elif choice == '9':
+            zomato.save_data(menu_file, orders_file)
 
-        elif choice == "0":
-            zomato.save_data()
-            print("Exiting...")
+        elif choice == '10':
+            zomato.load_data(menu_file, orders_file)
+
+        elif choice == '11':
             break
 
         else:
@@ -187,6 +219,9 @@ def main():
 
         print()
 
+    print("Exiting Zesty Zomato app.")
+    zomato.save_data(menu_file, orders_file)
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
